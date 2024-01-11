1c9ca05285e259c875051368b43b538bfa6dfc15
muchitto <trikafka@gmail.com>
2024-01-11 17:24:36 +0200

    Remove test and also fix annotation issues

--- Sources/Tests/Parsing.Tests/Annotations.cs
+++ Sources/Tests/Parsing.Tests/Annotations.cs
@@ -42,4 +42,41 @@ public class Annotations : ParserTest
             )
         ));
     }
+
+    [Fact]
+    public void Test_Function_With_A_Single_Annotation_With_No_Arguments_On_Same_Line_As_Declaration()
+    {
+        const string source = """
+                                  @Annotation func main()
+                                  end
+                              """;
+
+        var ast = Parser.Parse("test", source);
+
+        Assert.True(ast.TestEquals(
+            new ProgramContainerNode(Pos,
+                [
+                    new FunctionDeclarationNode(
+                        new IdentifierNode(Pos, "main"),
+                        [],
+                        new BodyContainerNode(Pos, [], false),
+                        false,
+                        false,
+                        null
+                    )
+                    {
+                        Annotations = new AnnotationsNode(
+                            Pos,
+                            [
+                                new AnnotationNode(
+                                    new IdentifierNode(Pos, "Annotation"),
+                                    []
+                                )
+                            ]
+                        )
+                    }
+                ]
+            )
+        ));
+    }
 }
